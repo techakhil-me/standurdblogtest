46961f259e6b8c982fd1d50e07418eb1f2d7ae01
TechAkhil <43501564+techakhilc47@users.noreply.github.com>
2020-07-23 10:14:30 +0530

    about sass

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -10,26 +10,15 @@ const About = () => {
       <SEO title="About" path="/about" />
       <article className="about">
         <img src={me} className="user-avatar" alt="Akhil Padmanabhan" />
-        <section className="about-context">
-          <div>
-          <div class="container">
-  <div class="upper">
-    <div class="upper-text">Hello</div>
-  </div>
-  <div class="lower">
-    <div class="lower-text">Hello</div>
-  </div>
-</div>
-<div class="divider"></div>
-            <p className="time">2003-03-07</p>
-            <p>
+          <div className="cont">
+            <h3 className="title">Akhil Padmanabhan</h3>
+            <p className="para">
             Tech Enthusiast, having designed many logos, banners and website ux/ui looking forward to enrich my creative hobby as an professional asset.
             My education also made me capable of coding programs in python, c++. I love competitive programming and writing automation scripts.
             Check out <a href="https://techakhilc47.github.io/">My Portfolio</a> to get in touch with me.
               
             </p>
           </div>
-        </section>
       </article>
     </Layout>
   );
